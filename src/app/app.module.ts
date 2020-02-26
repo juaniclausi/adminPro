5195840295b6a4ee773835b97093f95a8d7f2b49
Juan Ignacio <juaniclausi@gmail.com>
2020-02-26 19:41:59 -0300

    Modificaciones en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,33 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { PagesModule } from './pages/pages.module';
+
 import { AppComponent } from './app.component';
-import { NoPageFoundComponent } from './shared/no-page-found/no-page-found.component';
 import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumsComponent } from './shared/breadcrums/breadcrums.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,    
-    NoPageFoundComponent, 
-    LoginComponent, 
-    DashboardComponent, 
-    ProgressComponent, 
-    Graficas1Component, 
-    HeaderComponent, 
-    SidebarComponent, 
-    BreadcrumsComponent, PagesComponent, RegisterComponent
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    PagesModule,
+    AppRoutingModule   
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
